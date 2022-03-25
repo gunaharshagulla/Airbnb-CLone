ad0ac194d4d1e3816a55186b694c23e7a2d57525
gunaharshagulla <78554755+gunaharshagulla@users.noreply.github.com>
2022-03-25 23:39:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-/*
-Challenge: Build the Hero component.
-Check the Figma file for the design specifics.
-*/
+import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App() {
+            // <Hero />
     return (
         <div>
             <Navbar />
+            <Card />
         </div>
     )
 }
